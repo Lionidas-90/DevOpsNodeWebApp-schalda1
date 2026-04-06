1fe2f6228d83a9023928518151a876902622ab1b
David Schaltegger <dacid_sch15@hotmail.com>
2026-04-06 17:32:51 +0200

    LNW4 docker image

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course!');
+  const hostname = require('os').hostname();
+  res.send(`
+    <html>
+      <body style="font-family: sans-serif; padding: 2rem;">
+        <h1>Hello FS2026 DevOps Course!</h1>
+        <p>Container ID: <strong>${hostname}</strong></p>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, HOST);
